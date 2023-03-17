1ee19c6c18fe84b30123ef9084b6d2b819bd9906
Mehmet Can KALABAŞ <mehmetcankalabas.mk@gmail.com>
2023-03-17 17:03:34 +0300

    Add-Project

---
+++ Patika-PhoneBookApp/IPhoneBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patika_
+{
+    public interface IPhoneBook
+    {
+        void AddNewContact();
+
+        void DeleteContact();
+
+        void UpdateContact();
+
+        void ListAllContacts();
+
+        void SearchContact();
+
+        void PreferencesOrExitMenu();
+
+        void ExitPhoneBook();
+
+    }
+}
